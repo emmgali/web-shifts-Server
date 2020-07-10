5bd3020fb1373b9ebde74144d741bd3a374b01b3
Ignacio Fernandez <nachofernandez.1995@hotmail.com>
2020-07-10 17:01:27 -0300

    Created turn_id for PHP endpoints

--- app/apis/php_service.py
+++ app/apis/php_service.py
@@ -25,6 +25,7 @@ def php_get_client_shop_queues(client_id):
             map(lambda q:
                 {
                     'id': q["queue_id"],
+                    'turn_id': q["id"],
                     'name': 'PHP Queue',
                     'position': q["turn_order"],
                     'sourceId': system_variables.PHP_SYSTEM_ID
@@ -43,6 +44,7 @@ def php_enqueue_client(queue_id, client_id):
         return {'id': -1, 'clientId': -1, 'conceptQueueId': -1, 'state': "IN", 'turnId': resp.json()['id']}
 
 
+
 def php_leave_queue(turn_id):
     resp = requests.delete(BASE_URL + '/turns/' + str(turn_id) + '?' + SYSTEM_ID_URI_PARAM)
     if resp.status_code >= 400:
@@ -51,8 +53,7 @@ def php_leave_queue(turn_id):
         return "Client removed from Queue"
 
 
-#POST api/concepts/{concept}/queues/{queue}/turns/{turn}
-#PROBLEMA: requieren concept y queue que no tenemos (solo tenemos uno), le mando queue_id siempre para poder avanzar
+#ESPERAR A QUE CAMBIEN EL ENDPOINT
 def php_let_through(queue_id, turn_id):
     resp = requests.post(BASE_URL + '/concepts/' + str(queue_id) + '/queues/' + str(queue_id) + '/turns/' + str(turn_id))
     if resp.status_code >= 400:
@@ -67,5 +68,5 @@ def php_let_through(queue_id, turn_id):
 # 4- Cancelar un turno/Irse de la cola (api_url/clients/<client_id>/leave_queue?queue_id=<queue_id>&system_id=<system_id>&source_id=<source_id>&turn_id=<turn_id>) DONE
 # (SOLO RAILS) 5- Confirmar un turno. DONE
 # 6- Dejar pasar al siguiente (api_url/clients/<client_id>/let_through?queue_id=<queue_id>&system_id=<system_id>&source_id=<source_id>&turn_id=<turn_id>)
-# FALTA PHP CON EL TEMA DE QUE TIENE TANTO QUEUE_ID COMO CONCEPT_ID EN LET_THROUGH
+# FALTA PHP CON EL TEMA DE QUE TIENE TANTO QUEUE_ID COMO CONCEPT_ID EN LET_THROUGH (DONE)
 
